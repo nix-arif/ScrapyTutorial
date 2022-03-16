7aa4cd68cf3694e22fcff73fb471e109fc416898
nix.arif <nix.arif@gmail.com>
2022-03-16 10:30:28 +0800

    Initial commit

---
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -0,0 +1,10 @@
+from tracemalloc import start
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+  name = 'quotes'
+  start_urls = ['https://quotes.toscrape.com/']
+
+  def parse(self, response):
+    title = response.css('title::text').extract()
+    yield {'titletext': title}
